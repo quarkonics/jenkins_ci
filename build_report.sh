f84d1e018b1ab7dfad4337865e9d7004413e33b2
quarkonics <quarkonics@gmail.com>
2016-02-22 21:11:47 +0800

    Don't fail reporting

--- build_report.sh
+++ build_report.sh
@@ -9,7 +9,7 @@ fi
 DURATION_MIN=`echo "${DURATION}/60" | bc`
 DURATION_SEC=`echo "${DURATION}%60" | bc`
 rm -rf /tmp/${BUILD_TYPE}_report.json
-BIN_NAME=$(basename `ls zstack-utility/zstackbuild/target/*.bin`)
+BIN_NAME=$(basename `ls zstack-utility/zstackbuild/target/*.bin`) || echo "fail to find bin"
 echo "{\"fallback\":\"${BUILD_TYPE} - #${BUILD_NUMBER}(<http://192.168.200.1/mirror/${BUILD_TYPE}/${BUILD_NUMBER}/|Open>|<http://192.168.200.1/mirror/${BUILD_TYPE}/${BUILD_NUMBER}/${BIN_NAME}|Download>) ${BUILD_RESULT} after ${DURATION_MIN} min ${DURATION_MIN} sec (<http://192.168.200.1/mirror/${BUILD_TYPE}/${BUILD_NUMBER}/${BUILD_TYPE}.log|Log>)\",\"fields\":[{\"value\":\"${BUILD_TYPE} - #${BUILD_NUMBER}(<http://192.168.200.1/mirror/${BUILD_TYPE}/${BUILD_NUMBER}/|Open>|<http://192.168.200.1/mirror/${BUILD_TYPE}/${BUILD_NUMBER}/${BIN_NAME}|Download>)\",\"short\":true},{\"value\":\"${BUILD_RESULT} after ${DURATION_MIN} min ${DURATION_MIN} sec (<http://192.168.200.1/mirror/${BUILD_TYPE}/${BUILD_NUMBER}/${BUILD_TYPE}.log|Log>)\",\"short\":true}],\"color\":\"${RESULT_COLOR}\"}," >> /tmp/${BUILD_TYPE}_report.json
 BUILD_VERSIONS_FILE=${BUILD_TYPE}/latest/versions.txt
 
@@ -20,7 +20,7 @@ for i in $(seq `wc -l ${BUILD_VERSIONS_FILE}|awk '{print $1}'`); do
 	COMPONENT=`sed -n "${i}p" ${BUILD_VERSIONS_FILE} | awk '{print $1}' | awk -F ':' '{print $1}'`
 	COMMIT=`sed -n "${i}p" ${BUILD_VERSIONS_FILE} | awk '{print $2}'`
 	SUBJECT=`sed -n "${i}p" ${BUILD_VERSIONS_FILE} | awk '{for (i=2; i<NF; i++) printf("%s ",$i);print $i}' | sed 's/\"/\\\"/g'`
-	OLD_COMMIT=`grep "${COMPONENT}:" versions.txt.old | awk '{print $2}'`
+	OLD_COMMIT=`grep "${COMPONENT}:" versions.txt.old | awk '{print $2}'` || echo "could not find old commit"
 	if [ "${OLD_COMMIT}" == "${COMMIT}" ]; then
 		COLOR="AAAAAA"
 	else
