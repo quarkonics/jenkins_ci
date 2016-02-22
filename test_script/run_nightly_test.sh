13bdfa6a5e2cb7d10a45eb76124164fd9356f7a5
quarkonics <quarkonics@gmail.com>
2016-02-22 21:15:19 +0800

    Make nightly testing to create VM for testing dynamically

---
+++ test_script/run_nightly_test.sh
@@ -0,0 +1,173 @@
+IP=$1
+TEST_TARGET=$2
+OVERALL_BUILD_NUMBER=$3
+IP_RANGE_NAME=$4
+
+compare_result()
+{
+	E_TESTSUITE=$1
+	SUMMARYFILE=/home/${IP}/result_${IP}.summary
+	REFSUMMARYFILE=/home/${IP}/${E_TESTSUITE}.ref
+	for i in $(seq `wc -l ${SUMMARYFILE}|awk '{print $1}'`); do
+		TESTCASE=`sed -n "${i}p" ${SUMMARYFILE} | awk '{print $1}'`
+		CURRENT_RESULT=`sed -n "${i}p" ${SUMMARYFILE} | awk '{print $2}'`
+		REF_RESULT=`grep -w "${TESTCASE}" ${REFSUMMARYFILE} | head -1 | awk '{print $2}'`
+		if [ "${REF_RESULT}" == "" ]; then
+			REF_RESULT="N/A"
+		fi
+
+		if [ "${REF_RESULT}" == "${CURRENT_RESULT}" ]; then
+			continue
+		fi
+
+		echo "{\"fields\":[{\"value\":\"${TESTCASE}\",\"short\":true},{\"value\":\"${REF_RESULT}->${CURRENT_RESULT}\",\"short\":true}],\"color\":\"AAAAAA\"},"
+	done
+}
+
+if [ ${TEST_TARGET} == "build_zstack" -o ${TEST_TARGET} == "zstack_ci" ]; then
+	CI_TARGET=zstack_ci
+elif [ ${TEST_TARGET} == "build_mevoco" -o ${TEST_TARGET} == "mevoco_ci" ]; then
+	CI_TARGET=mevoco_ci
+fi
+
+TESTSUITES="basic virtualrouter virtualrouter(localstorage) virtualrouter(local+nfs)"
+CENTOS_REPO="alibase 163base internalbase"
+EPEL_REPO="aliepel epel"
+PASS_NUMBER=0
+TOTAL_NUMBER=0
+
+rm -rf /home/${IP}/report.${IP}.json
+rm -rf /home/${IP}/zstack-woodpecker/dailytest/config_xml/test-result/
+rm -rf /home/${IP}/config_xml/
+mkdir -p /home/${IP}/config_xml/
+rm -rf /home/${IP}/log_${IP}.tgz
+echo "{\"fields\":[{\"value\":\"zstack-woodpecker:\",\"short\":true},{\"value\":\"`cat /home/${IP}/zstack_woodpecker_version.txt`\",\"short\":true}],\"color\":\"${COLOR}\"}," >> /home/${IP}/report.${IP}.json
+for TS in ${TESTSUITES}; do
+	E_TS=`echo ${TS} | sed 's/(/_/' | sed 's/)//' | sed 's/+/_/'`
+	rm -rf /home/nfs/*
+	rm -rf /home/local-ps/*
+	rm -rf /home/sftpBackupStorage/*
+	rm -rf /home/${IP}/result_${IP}.summary
+	BASIC_TS=`echo ${TS} | awk -F '(' '{print $1}'`
+	BASIC_TS_CONF=`echo ${TS} | awk -F '(' '{print $2}' | awk -F ')' '{print $1}'`
+	TESTSUITE_DONE=0
+	for CR in ${CENTOS_REPO}; do
+		for ER in ${EPEL_REPO}; do
+			rsync -a /home/${IP}/zstack-woodpecker/dailytest/config_xml/ /home/${IP}/config_xml/ || echo "no log yet"
+			if [ ${TESTSUITE_DONE} -eq 1 ]; then
+				continue || echo continue
+			fi
+			echo "try use ${CR} ${ER} repo"
+			yum-config-manager --disable alibase > /dev/null
+			yum-config-manager --disable 163base > /dev/null
+			yum-config-manager --disable internalbase > /dev/null
+			yum-config-manager --disable epel > /dev/null
+			yum-config-manager --disable aliepel > /dev/null
+			yum-config-manager --enable ${CR} > /dev/null
+			yum-config-manager --enable ${ER} > /dev/null
+			yum clean metadata
+			INSTALL_VIM=success
+			yum --nogpgcheck install -y vim || INSTALL_VIM=failure
+			if [ ${INSTALL_VIM} == "failure" ]; then
+				echo "{\"fields\":[{\"value\":\"fail to setup testsuite ${TS} with yum repo:\",\"short\":true},{\"value\":\"${CR} and ${ER}\",\"short\":true}],\"color\":\"${COLOR}\"}," >> /home/${IP}/report.${IP}.json
+				continue || echo continue
+			fi
+			yum clean metadata
+			INSTALL_MARIADB=success
+			yum --nogpgcheck install -y mariadb mariadb-server || INSTALL_MARIADB=failure
+			if [ ${INSTALL_MARIADB} == "failure" ]; then
+				echo "{\"fields\":[{\"value\":\"fail to setup testsuite ${TS} with yum repo:\",\"short\":true},{\"value\":\"${CR} and ${ER}\",\"short\":true}],\"color\":\"${COLOR}\"}," >> /home/${IP}/report.${IP}.json
+				continue || echo continue
+			fi
+			
+			cd /home/${IP}/
+			rm -rf zstack-woodpecker zstack-utility
+			mkdir -p zstack-woodpecker
+			tar -x -C zstack-woodpecker -f zstack-woodpecker.tar
+			mkdir -p zstack-utility
+			tar -x -C zstack-utility -f zstack-utility.tar
+			
+			if [ ${CR} == "alibase" ]; then
+				CR1=aliyun
+			elif [ ${CR} == "163base" ]; then
+				CR1=163
+			fi
+			sed -i "s/aliyun/${CR1}/g" /home/${IP}/zstack-woodpecker/zstackwoodpecker/zstackwoodpecker/setup_actions.py
+			cd /home/${IP}/zstack-woodpecker/dailytest/
+			scp /home/${IP}/build_zstack.template.sh /home/${IP}/zstack-woodpecker/dailytest/build_zstack.sh
+			sed -i "s/TARGET_IP/${IP}/g" /home/${IP}/zstack-woodpecker/dailytest/build_zstack.sh
+			chmod a+x /home/${IP}/zstack-woodpecker/dailytest/build_zstack.sh
+			DEPLOY_ZSTACK=success
+			./zstest.py -b || DEPLOY_ZSTACK=failure
+			if [ ${DEPLOY_ZSTACK} == "failure" ]; then
+				echo "{\"fields\":[{\"value\":\"fail to setup testsuite ${TS} with yum repo:\",\"short\":true},{\"value\":\"${CR} and ${ER}\",\"short\":true}],\"color\":\"${COLOR}\"}," >> /home/${IP}/report.${IP}.json
+				continue || echo continue
+			fi
+			
+			cd ../tools/
+			sh copy_test_config_to_local.sh
+			scp /home/${IP}/deploy.vr.tmpt /root/.zstackwoodpecker/integrationtest/vm/deploy.tmpt
+			sed -i "s/TARGET_IP/${IP}/g" /root/.zstackwoodpecker/integrationtest/vm/deploy.tmpt
+			if [ "${IP_RANGE_NAME}" == "IP_RANGE1" ]; then
+				MANAGEMENT_IP_START="192.168.201.2"
+				MANAGEMENT_IP_END="192.168.201.11"
+				IP_START="192.168.201.12"
+				IP_END="192.168.201.21"
+			elif [ "${IP_RANGE_NAME}" == "IP_RANGE2" ]; then
+				MANAGEMENT_IP_START="192.168.201.22"
+				MANAGEMENT_IP_END="192.168.201.31"
+				IP_START="192.168.201.32"
+				IP_END="192.168.201.41"
+			fi
+			sed -i "s/MANAGEMENT_IP_START/${MANAGEMENT_IP_START}/g" /root/.zstackwoodpecker/integrationtest/vm/deploy.tmpt
+			sed -i "s/MANAGEMENT_IP_END/${MANAGEMENT_IP_END}/g" /root/.zstackwoodpecker/integrationtest/vm/deploy.tmpt
+			sed -i "s/IP_START/${IP_START}/g" /root/.zstackwoodpecker/integrationtest/vm/deploy.tmpt
+			sed -i "s/IP_END/${IP_END}/g" /root/.zstackwoodpecker/integrationtest/vm/deploy.tmpt
+
+			scp /home/${IP}/deploy.xml /root/.zstackwoodpecker/integrationtest/vm/virtualrouter/deploy.xml
+			scp /home/${IP}/deploy-local-ps.xml /root/.zstackwoodpecker/integrationtest/vm/virtualrouter/deploy-local-ps.xml
+			scp /home/${IP}/deploy-local-nfs.xml /root/.zstackwoodpecker/integrationtest/vm/virtualrouter/deploy-local-nfs.xml
+			cd /home/${IP}/zstack-woodpecker/dailytest/
+			rm -rf /home/${IP}/result_${IP}.log /home/${IP}/log_${IP}.tgz
+			RUN_BASIC=success
+			if [ "${BASIC_TS_CONF}" == "" ]; then
+				./zstest.py -s ${BASIC_TS} | tee /home/${IP}/result_${IP}.log || RUN_BASIC=failure
+			else
+				if [ "${BASIC_TS_CONF}" == "localstorage" ]; then
+					./zstest.py -s ${BASIC_TS} -C /root/.zstackwoodpecker/integrationtest/vm/${BASIC_TS}/test-config-local-ps.xml | tee /home/${IP}/result_${IP}.log || RUN_BASIC=failure
+				elif [ "${BASIC_TS_CONF}" == "local+nfs" ]; then
+					./zstest.py -s ${BASIC_TS} -C /root/.zstackwoodpecker/integrationtest/vm/${BASIC_TS}/test-config-local-nfs.xml | tee /home/${IP}/result_${IP}.log || RUN_BASIC=failure
+				fi
+			fi
+			cat /home/${IP}/result_${IP}.log | sed -n '/--*$/{:1;N;/--*$/{p;b};N;b1}' | grep -v '\-\-' | awk '{if ($1~/:/) {tttt=$1;gsub(":","", tttt)} else {if ($2==1) {printf("%s/%s PASS\n", tttt, $1, $2, $3, $4, $5)} else {if ($3==1) {printf("%s/%s FAIL\n", tttt, $1, $2, $3, $4, $5)} else {if ($4==1) {printf("%s/%s SKIP\n", tttt, $1, $2, $3, $4, $5)} else {if ($5==1) {printf("%s/%s TIMEOUT\n", tttt, $1, $2, $3, $4, $5)}}}}}}' > /home/${IP}/result_${IP}.summary
+			SUITE_SETUP=success
+			grep suite_setup /home/${IP}/result_${IP}.summary | grep PASS || SUITE_SETUP=failure
+			if [ ${SUITE_SETUP} == "failure" ]; then
+				echo "{\"fields\":[{\"value\":\"fail to setup testsuite ${TS} with yum repo:\",\"short\":true},{\"value\":\"${CR} and ${ER}\",\"short\":true}],\"color\":\"${COLOR}\"}," >> /home/${IP}/report.${IP}.json
+				continue || echo continue
+			else
+				echo "{\"fields\":[{\"value\":\"setup testsuite ${TS} with yum repo:\",\"short\":true},{\"value\":\"${CR} and ${ER}\",\"short\":true}],\"color\":\"${COLOR}\"}," >> /home/${IP}/report.${IP}.json
+			fi
+	
+			TS_TOTAL_NUMBER=`cat /home/${IP}/result_${IP}.summary | wc -l`
+			TS_PASS_NUMBER=`cat /home/${IP}/result_${IP}.summary | grep -w PASS | wc -l`
+			let PASS_NUMBER=${PASS_NUMBER}+${TS_PASS_NUMBER}
+			let TOTAL_NUMBER=${TOTAL_NUMBER}+${TS_TOTAL_NUMBER}
+			echo "{\"fields\":[{\"value\":\"${TS}:\",\"short\":true},{\"value\":\"PASS/TOTAL=${TS_PASS_NUMBER}/${TS_TOTAL_NUMBER}\",\"short\":true}],\"color\":\"${COLOR}\"}," >> /home/${IP}/report.${IP}.json
+#			cat /home/${IP}/result_${IP}.log  | sed -n '/--*$/{:1;N;/--*$/{p;b};N;b1}' | grep -v '\-\-' | awk '{if ($1~/:/) {tttt=$1;gsub(":","", tttt)} else {if ($2!=1) {printf("{\"fields\":[{\"value\":\"%s/%s\",\"short\":true},{\"value\":\"FAIL\",\"short\":true}],\"color\":\"F35A00\"},", tttt, $1, $2, $3, $4, $5)}}}' >> /home/${IP}/report.${IP}.json
+#			cat /home/${IP}/result_${IP}.summary | grep -v PASS | awk '{printf("{\"fields\":[{\"value\":\"%s\",\"short\":true},{\"value\":\"%s\",\"short\":true}],\"color\":\"F35A00\"},", $1, $2)}' >> /home/${IP}/report.${IP}.json
+			compare_result ${E_TS} >> /home/${IP}/report.${IP}.json
+			TESTSUITE_DONE=1
+		done
+	done
+#	curl -X POST --data-urlencode "payload={\"text\" : \"Nightly result(<http://192.168.200.1/mirror/${CI_TARGET}/${OVERALL_BUILD_NUMBER}/log.tgz|Log>) against ${TEST_TARGET} - #${OVERALL_BUILD_NUMBER}(<http://192.168.200.1/mirror/${CI_TARGET}/${OVERALL_BUILD_NUMBER}/|Open>)PASS/TOTAL=${PASS_NUMBER}/${TOTAL_NUMBER}\", \"username\" : \"jenkins\", \"attachments\" : [`cat /home/${IP}/report.${IP}.json`{}]}" https://hooks.slack.com/services/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
+	rsync -a /home/${IP}/zstack-woodpecker/dailytest/config_xml/ /home/${IP}/config_xml/
+	tar czh /home/${IP}/config_xml/test-result/latest > /home/${IP}/log_${IP}.tgz
+	mkdir -p ${CI_TARGET}/${OVERALL_BUILD_NUMBER}/
+	cp /home/${IP}/log_${IP}.tgz ${CI_TARGET}/${OVERALL_BUILD_NUMBER}/nightly_log.tgz
+	scp -r ${CI_TARGET}/${OVERALL_BUILD_NUMBER} 192.168.200.1:/httpd/${CI_TARGET}/
+	scp /home/${IP}/result_${IP}.summary 192.168.200.1:/httpd/${CI_TARGET}/${OVERALL_BUILD_NUMBER}
+	scp /home/${IP}/result_${IP}.summary 192.168.200.1:/httpd/${CI_TARGET}/${E_TS}.ref
+done
+#curl -X POST --data-urlencode "payload={\"text\" : \"Nightly result(<http://192.168.200.1/mirror/${CI_TARGET}/${OVERALL_BUILD_NUMBER}/log.tgz|Log>) against ${TEST_TARGET} - #${OVERALL_BUILD_NUMBER}(<http://192.168.200.1/mirror/${CI_TARGET}/${OVERALL_BUILD_NUMBER}/|Open>)PASS/TOTAL=${PASS_NUMBER}/${TOTAL_NUMBER}\", \"username\" : \"jenkins\", \"attachments\" : [`cat /home/${IP}/report.${IP}.json`{}]}" https://hooks.slack.com/services/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
+curl -X POST --data-urlencode "payload={\"text\" : \"Nightly result(<http://192.168.200.1/mirror/${CI_TARGET}/${OVERALL_BUILD_NUMBER}/log.tgz|Log>) against ${TEST_TARGET} - #${OVERALL_BUILD_NUMBER}(<http://192.168.200.1/mirror/${CI_TARGET}/${OVERALL_BUILD_NUMBER}/|Open>)PASS/TOTAL=${PASS_NUMBER}/${TOTAL_NUMBER}\", \"username\" : \"jenkins\", \"attachments\" : [`cat /home/${IP}/report.${IP}.json`{}]}" https://hooks.slack.com/services/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
--- test_script/run_remote_test.sh
+++ test_script/run_remote_test.sh
@@ -10,9 +10,11 @@ setup_no_password()
 }
 
 BUILD_TYPE=$1
-OVERALL_BUILD_NUMBER=$2
+TEST_TYPE=$2
+OVERALL_BUILD_NUMBER=$3
+IP_RANGE_NAME=$4
 SESSION_UUID=$(zstack_login)
-VM_UUID=$(zstack_create_vm ${SESSION_UUID} jenkins_bat_${BUILD_TYPE})
+VM_UUID=$(zstack_create_vm ${SESSION_UUID} jenkins_${TEST_TYPE}_${BUILD_TYPE})
 VM_IP=$(zstack_query_vm ${SESSION_UUID} ${VM_UUID} '["inventories"][0]["vmNics"][0]["ip"]')
 
 START_TIME=${SECONDS}
@@ -34,10 +36,18 @@ setup_no_password ${VM_IP}
 ssh root@${VM_IP} date
 ssh root@${VM_IP} rm -rf /home/${VM_IP}/
 ssh root@${VM_IP} mkdir -p /home/${VM_IP}/
-scp /var/lib/jenkins/test_script/prepare.sh root@${VM_IP}:/home/${VM_IP}/
-scp /var/lib/jenkins/test_script/run_test.sh root@${VM_IP}:/home/${VM_IP}/
+if [ "${TEST_TYPE}" == "bat" ]; then
+	scp /var/lib/jenkins/test_script/prepare.sh root@${VM_IP}:/home/${VM_IP}/
+	scp /var/lib/jenkins/test_script/run_test.sh root@${VM_IP}:/home/${VM_IP}/
 
-ssh root@${VM_IP} bash -ex /home/${VM_IP}/prepare.sh ${BUILD_TYPE} ${VM_IP}
-ssh root@${VM_IP} bash -ex /home/${VM_IP}/run_test.sh ${VM_IP} ${BUILD_TYPE} ${OVERALL_BUILD_NUMBER}
+	ssh root@${VM_IP} bash -ex /home/${VM_IP}/prepare.sh ${BUILD_TYPE} ${VM_IP}
+	ssh root@${VM_IP} bash -ex /home/${VM_IP}/run_test.sh ${VM_IP} ${BUILD_TYPE} ${OVERALL_BUILD_NUMBER}
+elif [ "${TEST_TYPE}" == "nightly" ]; then
+	scp /var/lib/jenkins/test_script/prepare_nightly.sh root@${VM_IP}:/home/${VM_IP}/
+	scp /var/lib/jenkins/test_script/run_nightly_test.sh root@${VM_IP}:/home/${VM_IP}/
+
+	ssh root@${VM_IP} bash -ex /home/${VM_IP}/prepare_nightly.sh ${BUILD_TYPE} ${VM_IP}
+	ssh root@${VM_IP} bash -ex /home/${VM_IP}/run_nightly_test.sh ${VM_IP} ${BUILD_TYPE} ${OVERALL_BUILD_NUMBER} ${IP_RANGE_NAME}
+fi
 zstack_destroy_vm ${SESSION_UUID} ${VM_UUID}
 zstack_logout ${SESSION_UUID}
